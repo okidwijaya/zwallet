5848826666d306db1988f4469dfe9acd1642e768
okidwijaya <okidwijaya@gmail.com>
2022-02-09 13:15:58 +0700

    deploy

--- src/pages/topup/index.js
+++ src/pages/topup/index.js
@@ -1,15 +1,19 @@
 import Layout from "src/common/components/Layout";
 import styles from "src/common/styles/Account.module.css";
-import Link from "next/Link";
+import Link from "next/link";
 
 export default function Topup() {
   return (
     <>
       <Layout>
-        <p><strong>TopUp</strong></p>
+        <p>
+          <strong>TopUp</strong>
+        </p>
         <p>Enter the amount of money, and click submit</p>
         <form className={styles.login}>
-          <div className={`${styles["form-input-warpper"]} form-group w-25 mx-auto mt-5 p-5`}>
+          <div
+            className={`${styles["form-input-warpper"]} form-group w-25 mx-auto mt-5 p-5`}
+          >
             <div
               className={`${styles["style-input"]} d-flex justify-content-center`}
             >
@@ -21,15 +25,15 @@ export default function Topup() {
                 required
               />
             </div>
-          <Link href="/dashboard" passHref>
-          <button
-              type="submit"
-              className="btn btn-block btn-secondary w-100"
+            <Link href="/dashboard" passHref>
+              <button
+                type="submit"
+                className="btn btn-block btn-secondary w-100"
               >
-              Submit
-            </button>
-          </Link>
-              </div>
+                Submit
+              </button>
+            </Link>
+          </div>
         </form>
       </Layout>
     </>
