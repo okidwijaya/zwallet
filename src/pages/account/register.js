1366171883ee453d8f6f0ff2d7b5ba279069fb2a
unknown <okidwijaya@gmail.com>
2022-04-12 21:52:15 +0700

    auth fixing

--- src/pages/account/register.js
+++ src/pages/account/register.js
@@ -17,8 +17,8 @@ export default function Register() {
   const registerHandler = (event) => {
     event.preventDefault();
     const body = {
-      // firstName: event.target.firstName.value,
-      // lastName: event.target.lastName.value,
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
       email: event.target.email.value,
       password: event.target.password.value,
     };
